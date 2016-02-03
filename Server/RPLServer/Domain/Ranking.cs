71b74622c32a1f00043ff38c0681e6337d8509b7
Scott Stromberg <srstromberg@gmail.com>
2016-02-02 19:39:29 -0700

    .
    put projects into their own repository. deleted server from this one.

--- Server/RPLServer/Domain/Ranking.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain
-{
-    public class Ranking
-    {
-        public RankType RankType { get; set; }
-        public int Rank { get; set; }
-        public decimal Wealth { get; set; }
-    }
-}
--- Server/RPLServer/Server/Models/CreatePersonRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Server.Models
-{
-    public class CreatePersonRequest
-    {
-        public string Name { get; set; }
-    }
-}
